39a670260f5549f4011ad5f126febd4d0d0f6b90
Simhadri <simhadri1998@gmail.com>
2022-08-18 17:02:13 +0200

    removed full python path with venv

--- projinit.bat
+++ projinit.bat
@@ -10,8 +10,8 @@ If "%1"=="" (
     echo "error"
 ) else (
     if "%3"=="" (
-        conda activate dllab & C:\Users\simha\miniconda3\envs\dllab\python.exe main.py %fn% %pl% 
+        python main.py %fn% %pl% 
     ) else (
-        conda activate dllab & C:\Users\simha\miniconda3\envs\dllab\python.exe main.py %fn% %pl% %flags%
+        python main.py %fn% %pl% %flags%
     )
 )
