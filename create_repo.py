8715b6a4802a83f609419b421edce55fed30a5f0
Simhadri <simhadri1998@gmail.com>
2022-08-18 16:46:58 +0200

    function to create remote repo

--- create_repo.py
+++ create_repo.py
@@ -3,21 +3,13 @@ from pathlib import Path
 import shutil
 from subprocess import Popen, check_call
 
+from github import Github
 
-def check_and_format_path(path):
+
+def create_project_dir(root, dir_path, project_name, prog_lang):
     '''
-    check if path contains any spaces and if it does,
-    enclose it in double quotes
+    create local project directory
     '''
-    if ' ' in path:
-        path = path.split('\\')
-        path = '/'.join(path)
-        path = f'"{path}"'
-    return path
-
-
-def create_local_repo(root, dir_path, project_name, prog_lang):
-    ''' create local repo '''
 
     # concatenate project path and project name to get full path
     proj_path = dir_path / project_name
@@ -28,36 +20,96 @@ def create_local_repo(root, dir_path, project_name, prog_lang):
     else:
         os.makedirs(proj_path)
     
-    # change directory to project directory
-    os.chdir(proj_path)
-
     # set path to .gitignore file
     dst = proj_path / '.gitignore'
     # check type of prog_lang and copy .gitignore file
     if prog_lang in ['python', 'py', 'p']:
         src = root / "gitignores/python.gitignore"
         shutil.copy(src, dst)
-        # open VS Code for python project
-        Popen('code .', shell=True)
     elif prog_lang in ['unity', 'un', 'u']:
         src = root / "gitignores/unity.gitignore"
         shutil.copy(src, dst)
 
+
+def _check_and_format_path(path):
+    '''
+    check if path contains any spaces and if it does,
+    enclose it in double quotes
+    '''
+
+    if ' ' in path:
+        path = path.split('\\')
+        path = '/'.join(path)
+        path = f'"{path}"'
+    return path
+
+
+def _check_and_open_app(proj_path, prog_lang):
+    # check type of prog_lang and open the application
+    if prog_lang in ['python', 'py', 'p']:
+        # open VS Code for python project
+        Popen('code .', shell=True)
+    elif prog_lang in ['unity', 'un', 'u']:
         # get path to Unity editor application from .env file
-        unity_path = check_and_format_path(os.getenv('UNITY_PATH'))
-        p_path = check_and_format_path(str(proj_path))
+        unity_path = _check_and_format_path(os.getenv('UNITY_PATH'))
+        p_path = _check_and_format_path(str(proj_path))
         if unity_path != None:
             # open Unity editor for project
-            check_call(f'{unity_path} -createProject {p_path}', shell=True)
+            Popen(f'{unity_path} -createProject {p_path}', shell=True)
         else:
             print("Please provide the path to Unity Editor")
-    
+
+
+def create_local_repo(dir_path, project_name, prog_lang):
+    '''
+    create local repo
+    '''
+
+    # concatenate project path and project name to get full path
+    proj_path = dir_path / project_name    
+    # change directory to project directory
+    os.chdir(proj_path)
+
+    commands = [
+        f'echo # {project_name} > README.md',
+        'git init',
+        'git add .',
+        'git commit -m "Initial commit"',
+    ]
     # create README file and initial commit
-    Popen(f'echo # {project_name} > README.md', shell=True)
-    check_call('git init', shell=True)
-    check_call('git add .', shell=True)
-    check_call('git commit -m "first commit"', shell=True)
+    for c in commands:
+        check_call(c, shell=True)
+
+    _check_and_open_app(proj_path, prog_lang)
 
 
-def create_remote_repo():
-    pass
+def create_remote_repo(token, dir_path, project_name, prog_lang):
+    '''
+    create remote repo
+    '''
+
+    # concatenate project path and project name to get full path
+    proj_path = dir_path / project_name    
+    # change directory to project directory
+    os.chdir(proj_path)
+
+    # initialize Github instance
+    g = Github(token)
+    user = g.get_user()
+    login = user.login
+    # create repository
+    repo = user.create_repo(project_name, private=True)
+
+    commands = [
+        f'echo # {repo.name} > README.md',
+        'git init',
+        f'git remote add origin https://github.com/{login}/{project_name}.git',
+        'git add .',
+        'git commit -m "Initial commit"',
+        'git push -u origin master'
+    ]
+    # create README file and initial commit
+    for c in commands:
+        check_call(c, shell=True)
+    
+    _check_and_open_app(proj_path, prog_lang)
--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-from create_repo import create_local_repo, create_remote_repo
+import create_repo as cr
+
 
 root = Path(os.getcwd())
 env_path = root / '.env'
 load_dotenv(dotenv_path=env_path)
 
+# python main.py <project_name> <prog_lang> <remote>
 project_name = str(sys.argv[1])
 prog_lang = str(sys.argv[2])
 # path to projects directory in .env file
@@ -18,13 +20,14 @@ token = os.getenv('GITHUB_TOKEN')
 
 
 def main():
+    cr.create_project_dir(root, dir_path, project_name, prog_lang)
     # check if project to create is local or remote
     # if number of arguments is 3, create remote repo
     if len(sys.argv) == 4:
-        create_remote_repo()
-        print("initialising remote repo")
+        print("Initialising remote repo")
+        cr.create_remote_repo(token, dir_path, project_name, prog_lang)
     else:
-        create_local_repo(root, dir_path, project_name, prog_lang)
+        cr.create_local_repo(dir_path, project_name, prog_lang)
 
 
 if __name__ == "__main__":
